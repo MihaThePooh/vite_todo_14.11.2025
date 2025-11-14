8444ef9ae26f95bea7802208446d93b627f5f023
home comp <mihailperov90@gmail.com>
2025-11-14 14:22:54 +0300

    add all files

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import './App.css'
+import {TodolistItem} from './TodolistItem'
+import {useState} from "react";
+
+export type Task = {
+    id: number
+    title: string
+    isDone: boolean
+}
+export type FilterValues = 'all' | 'active' | 'completed'
+
+export const App = () => {
+    const [tasks, setTasks] = useState<Task[]>([
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+        {id: 5, title: 'Typescript', isDone: false},
+        {id: 6, title: 'RTK query', isDone: false},
+    ])
+    const [filter, setFilter] = useState<FilterValues>('all')
+
+    const changeFilter = (filter: FilterValues) => {
+        setFilter(filter)
+    }
+
+    const deleteTask = (taskId: number) => {
+        const filteredTasks = tasks.filter(task => {
+            return task.id !== taskId
+        })
+        setTasks(filteredTasks)
+    }
+
+    let filteredTasks = tasks
+    if (filter === 'active') {
+        filteredTasks = tasks.filter(task => !task.isDone)
+    }
+    if (filter === 'completed') {
+        filteredTasks = tasks.filter(task => task.isDone)
+    }
+
+    return (
+        <div className="app">
+            <TodolistItem title="What to learn"
+                          deleteTask={deleteTask}
+                          changeFilter={changeFilter}
+                          tasks={filteredTasks}/>
+        </div>
+    )
+}
